1046196144ac6fb13dc32465d5e09def8a7c54de
李彦 <844698022@qq.com>
2023-05-11 09:54:47 +0800

    ✨feat: add playwright-chromium dep

--- demo/1-demo/index.vue
+++ demo/1-demo/index.vue
@@ -1,11 +1,12 @@
 <template>
-    <div>
-        <h1>1-demo</h1>
-    </div>
+  <div>
+    <h1>1-demo</h1>
+  </div>
 </template>
-    
+
 <script lang="ts" setup>
+interface Props {
+  test: string;
+}
 </script>
-<style lang="less" scoped>
-    
-</style>
+<style lang="less" scoped></style>
